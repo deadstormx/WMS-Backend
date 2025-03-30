8a15adc98b6e718d06aade523ccedb95b48d079f
Niranajan <shresthaniranjan0@gmail.com>
2025-03-30 22:00:51 +0545

    conrs added

--- register.js
+++ register.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 const userRoutes = require('./src/routes/userRoutes');
 const connectDB = require('./src/db/db');
 const requestLogger = require('./src/middlewares/requestLogger');
 require('dotenv').config();
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(requestLogger);
