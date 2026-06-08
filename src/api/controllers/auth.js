0d96f05b7125b22c14a55e1122bd783f1580d84c
Bibek Shrestha <bibekshrestha378@gmail.com>
2026-06-08 18:01:31 +0545

    fix: replace bcrypt with bcryptjs

--- src/api/controllers/auth.js
+++ src/api/controllers/auth.js
@@ -1,5 +1,5 @@
 const User = require('../../models/User');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken'); // Import jsonwebtoken
 
 // Helper function to generate JWT
